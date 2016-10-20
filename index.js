498be5e7b7399da63574b9a7af521dd89425ebe9
echosoar <82163514@qq.com>
2016-10-20 22:52:35 +0800

    update@161020

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ let hostToIp = host => {
 	if(/^(?:[\w][\w-]*[\w]\.|[\w]\.)+(?:[\w]{2,})$/.test(host)){
 		try {
 			let pingRes = child_process.execSync("ping "+host+" -l 0 -n 1").toString();
-			let ip = /(\d{1,3}\.){3}\d{1,3}/.exec(pingRes)[0];
-			return ip;
+			return  /(\d{1,3}\.){3}\d{1,3}/.exec(pingRes)[0];
 		} catch(e) {
 			return false;
 		}
